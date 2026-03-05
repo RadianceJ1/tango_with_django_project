1ff78d5a79f075e74acab72987dc8be0ae404b02
“Radiance <radiance@Radiances-MacBook-Air.local>
2026-03-05 23:34:56 +0000

    chapter 4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
@@ -26,4 +27,4 @@ urlpatterns = [
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
     
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
