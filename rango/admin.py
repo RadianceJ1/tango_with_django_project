189529465149447f03e356239f7709a2f3970e61
“Radiance <radiance@Radiances-MacBook-Air.local>
2026-03-07 17:00:13 +0000

    chapter 6 completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-
-# Register your models here.
 from rango.models import Category, Page
 
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('title','category', 'url')
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Category)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+# Update the registration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
